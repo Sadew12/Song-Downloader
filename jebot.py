b1e2044c778ccfc392980b36f3e5c69413b7b032
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-05-06 16:49:01 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -60,7 +60,7 @@ class AioHttp:
  #For private messages        
  #Ignore commands
  #No bots also allowed
-@Jebot.on_message(filters.private & ~filters.bot & ~filters.command("help") & ~filters.command("start") & ~filters.command("s"))  
+@Jebot.on_message(filters.private & ~filters.bot & ~filters.command("help") & ~filters.command("start") & ~filters.command("[s],[song]"))  
 #Lets Keep this Simple
 async def song(client, message):
   # Hope this will fix the args issue
@@ -81,12 +81,12 @@ async def song(client, message):
     #if args.startswith("/help"):
         #return ""    
     status = await message.reply(
-             text="<b>Downloading your song, Plz wait 🥺\n\nMade by @GalaxyLanka 🇱🇰</b>",
+             text="<b>Downloading your song, Plz wait 😉 \n\n🎶•••🎵•••🎧•••</b>",
              disable_web_page_preview=True,
                         reply_markup=InlineKeyboardMarkup(
                             [[
                                     InlineKeyboardButton(
-                                        "Developer 👨‍💻", url="https://t.me/Prabha_sha")
+                                        "𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙞𝙣𝙜 •••", url="https://t.me/GalaxyLanka")
                                 ]]
                         ),
                parse_mode="html",
@@ -119,21 +119,22 @@ async def song(client, message):
     
     
     
-@Jebot.on_message(filters.command("s"))
+@Jebot.on_message(filters.command("[s],[song],[audio],[music]"))
 async def song(client, message):
     message.chat.id
     user_id = message.from_user["id"]
     args = get_arg(message) + " " + "song"
     if args.startswith(" "):
-        await message.reply("<b>Enter a song name❗\n\nExample: `/s guleba`</b>")
+        await message.reply("<b>Enter a song name❗\n\nExample: `/s Shape of you`</b>")
         return ""
     status = await message.reply(
-             text="<b>Downloading your song, Plz wait 🥺\n\nMade by @GalaxyLanka 🇱🇰</b>",
+             text="<b>Downloading your song, Plz wait... 😉 \n\n🎶•••🎵•••🎧•••</b>",
              disable_web_page_preview=True,
                         reply_markup=InlineKeyboardMarkup(
                             [[
+                             
                                     InlineKeyboardButton(
-                                        "Developer", url="https://t.me/Prabha_sha")
+                                        "𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙞𝙣𝙜 •••", url="https://t.me/Galaxylanka")
                                 ]]
                         ),
                parse_mode="html",
@@ -170,16 +171,14 @@ async def start(client, message):
        await Jebot.send_message(
                chat_id=message.chat.id,
                text="""<b>Hey There, I'm a Song Downloader Bot 🎶
-
 Made by @Prabha_sha 🇱🇰
-
 Hit help button to find out more about how to use me</b>""",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
                                             "Help", callback_data="help"),
                                         InlineKeyboardButton(
-                                            "Group 🏘", url="https://t.me/GalaxyLanka")
+                                            "Group 🏘", url="https://t.me/HiTechRocket")
                                     ]]
                             ),        
             disable_web_page_preview=True,        
@@ -209,14 +208,13 @@ async def help(client, message):
         await Jebot.send_message(
                chat_id=message.chat.id,
                text="""<b>Send a song name to download song
-
 ~ @GalaxyLanka</b>""",
             reply_to_message_id=message.message_id
         )
     else:
         await Jebot.send_message(
                chat_id=message.chat.id,
-               text="<b>Song Downloader Help\n\nEnter a song name❗\n\nExample: `/s guleba`</b>",
+               text="<b>Song Downloader Help\n\nEnter a song name 🎶\n\nExample: `/s Shape of you`</b>",
             reply_to_message_id=message.message_id
         )     
         
@@ -231,8 +229,7 @@ async def button(Jebot, update):
 print(
     """
 Bot Started!
-
-Join @GalaxyLanka
+Join @HiTechRockets
 """
 )
 
